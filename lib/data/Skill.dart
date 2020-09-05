60dde4e8ed14bbe338332e157df0b1327a6763ad
psabak <peter.sabak@niceactimize.com>
2020-09-06 01:35:44 +0200

    Adding fromJson conversion support for Skill data object

--- lib/data/Skill.dart
+++ lib/data/Skill.dart
@@ -1,13 +1,21 @@
-class Skill {
+class Skill{
   final String id;
   final String name;
   final String description;
   final String imageURL;
-  //Constructor
+
   const Skill({
     this.id,
     this.name,
     this.description,
     this.imageURL
   });
+
+  factory Skill.fromJson(Map<String, dynamic> json) => Skill(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'],
+      imageURL: json['imageUrl']
+    );
+
 }
