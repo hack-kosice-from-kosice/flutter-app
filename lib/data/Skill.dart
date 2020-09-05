fa9e8a0744203129e451356411a050dc990a092f
psabak <peter.sabak@niceactimize.com>
2020-09-05 22:55:21 +0200

    Added SkillsPage StatefulWidget for showing all available skills

---
+++ lib/data/Skill.dart
@@ -0,0 +1,13 @@
+class Skill {
+  final String id;
+  final String name;
+  final String description;
+  final String imageURL;
+  //Constructor
+  const Skill({
+    this.id,
+    this.name,
+    this.description,
+    this.imageURL
+  });
+}
