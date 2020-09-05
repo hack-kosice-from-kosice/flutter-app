17b3fb2199f6b46f80c9b0f4f9d8a571a924fea6
psabak <peter.sabak@niceactimize.com>
2020-09-06 01:33:43 +0200

    Changed data

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -62,69 +62,33 @@ var dailyTasks = [
 var skills = [
   new Skill(
       id: '1',
-      name: 'Rosa rubiginosa',
+      name: 'Water',
       description: 'The Swedish botanist Carl Linnaeus',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Rosa_rubiginosa_1.jpg/440px-Rosa_rubiginosa_1.jpg'
+      imageURL: 'https://media-public.canva.com/MADpjrmJn8k/1/screen.svg'
   ),
   new Skill(
       id: '2',
-      name: 'Botany',
+      name: 'NoTvOrPcOrSmartphone',
       description: 'The leaves are borne alternately on the stem',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Rose_hip_02_ies.jpg'
+      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/No-TV.svg/1200px-No-TV.svg.png'
   ),
   new Skill(
       id: '3',
-      name: 'Rose leaflets',
+      name: 'Exercise',
       description: 'Subgenus, sometimes incorrectly called Eurosa',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/Roseleaves3800px.JPG'
+      imageURL: 'https://media-public.canva.com/MADrPCb9Hik/1/screen.svg'
   ),
   new Skill(
       id: '4',
-      name: 'Amber Flush',
+      name: 'Sun',
       description: 'Rose perfumes are made from rose oil',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Rose_Amber_Flush_20070601.jpg/440px-Rose_Amber_Flush_20070601.jpg'
+      imageURL: 'https://media-public.canva.com/MADZ4yTWnmA/1/screen.svg'
   ),
   new Skill(
       id: '5',
-      name: 'Rosa gallica',
+      name: 'No Coffee',
       description: 'Rosa gallica Evêque, painted by Redouté',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Redoute_-_Rosa_gallica_purpuro-violacea_magna.jpg/440px-Redoute_-_Rosa_gallica_purpuro-violacea_magna.jpg'
-  ),
-  new Skill(
-      id: '6',
-      name: 'Easter Lily',
-      description: 'Reproductive parts of Easter Lily.',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Lillium_Stamens.jpg/440px-Lillium_Stamens.jpg'
-  ),
-  new Skill(
-      id: '7',
-      name: 'calla lily',
-      description: 'The familiar calla lily is not a single flower.',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/White_and_yellow_flower.JPG/440px-White_and_yellow_flower.JPG'
-  ),
-  new Skill(
-      id: '8',
-      name: 'Crateva religiosa',
-      description: 'this Crateva religiosa flower has both stamens',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Crateva_religiosa.jpg/440px-Crateva_religiosa.jpg'
-  ),
-  new Skill(
-      id: '9',
-      name: 'Amborella trichopoda',
-      description: 'Amborella trichopoda may have features',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Amborella_trichopoda_%283065968016%29_fragment.jpg/440px-Amborella_trichopoda_%283065968016%29_fragment.jpg'
-  ),
-  new Skill(
-      id: '10',
-      name: 'Lilies',
-      description: 'Lilies are often used to denote life or resurrection',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Liliumbulbiferumflowertop.jpg/440px-Liliumbulbiferumflowertop.jpg'
-  ),
-  new Skill(
-      id: '11',
-      name: 'Bee orchid',
-      description: 'A Bee orchid has evolved over many generations',
-      imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Ophrys_apifera_flower1.jpg/340px-Ophrys_apifera_flower1.jpg'
-  ),
+      imageURL: 'https://image.flaticon.com/icons/png/512/2029/2029313.png'
+  )
 
 ];
